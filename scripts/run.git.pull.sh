a307ec36b1322467ba3ed728305d641ac08f4ce8
Robin Östlund <robin@kiisnet.se>
2017-12-27 13:53:18 +0100

    updates

--- scripts/run.git.pull.sh
+++ scripts/run.git.pull.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 . /usr/local/bin/sources.sh
 
-if [ ! -z $GIT_WEBSITE_REPO ]; then
-  # clone git repo
-  if [ ! -z $GIT_WEBSITE_BRANCH ]; then
-    cd /var/www_git
-    git pull -q origin $GIT_WEBSITE_BRANCH
-  else
-    cd /var/www_git
-    git pull -q origin master
-  fi
-  chown -R www-data:www-data /var/www_git
-fi
+cd /var/www_git
+git pull -q
+
+chown -R www-data:www-data /var/www_git
