609826b44764737ee5ab8be6ce5531c3006e4fd6
Robin Östlund <robin@kiisnet.se>
2017-12-27 12:37:47 +0100

    updates

--- scripts/run.git.pull.sh
+++ scripts/run.git.pull.sh
@@ -4,19 +4,13 @@
 if [ ! -z $GIT_WEBSITE_REPO ]; then
   # clone git repo
   if [ ! -z $GIT_WEBSITE_BRANCH ]; then
-    git clone -b $GIT_WEBSITE_BRANCH $GIT_WEBSITE_REPO /var/www_git
-    GIT_EXIT_CODE=$?
+    git fetch --all
+    git reset --hard origin/$GIT_WEBSITE_BRANCH
+    #git clone -b $GIT_WEBSITE_BRANCH $GIT_WEBSITE_REPO /var/www_git
+    #GIT_EXIT_CODE=$?
   else
-    git clone $GIT_WEBSITE_REPO /var/www_git
-    GIT_EXIT_CODE=$?
+    git fetch --all
+    git reset --hard origin/master
+    #GIT_EXIT_CODE=$?
   fi
-
-  # do some stuff if git clone was successful
-  if [[ $GIT_EXIT_CODE -eq 0 ]]; then
-    echo "git pull successful of $GIT_WEBSITE_REPO:$GIT_WEBSITE_BRANCH"
-  else
-    echo "git pull failed of $GIT_WEBSITE_REPO:$GIT_WEBSITE_BRANCH"
-  fi
-
-
 fi
