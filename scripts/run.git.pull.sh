dad6867f4d3ddc2ae0205500e4d13a81d4c38f31
Robin Östlund <robin@kiisnet.se>
2017-12-27 12:45:47 +0100

    updates

--- scripts/run.git.pull.sh
+++ scripts/run.git.pull.sh
@@ -4,13 +4,10 @@
 if [ ! -z $GIT_WEBSITE_REPO ]; then
   # clone git repo
   if [ ! -z $GIT_WEBSITE_BRANCH ]; then
-    git fetch --all
-    git reset --hard origin/$GIT_WEBSITE_BRANCH
-    #git clone -b $GIT_WEBSITE_BRANCH $GIT_WEBSITE_REPO /var/www_git
-    #GIT_EXIT_CODE=$?
+    cd /var/www_git
+    git pull -q origin $GIT_WEBSITE_BRANCH
   else
-    git fetch --all
-    git reset --hard origin/master
-    #GIT_EXIT_CODE=$?
+    cd /var/www_git
+    git pull -q origin master
   fi
 fi
